354f67b2251eaabc7fec95ab0c0d5d94933772c7
redblackcoder <redblackcoder@users.noreply.github.com>
2025-05-31 16:16:03 +0000

    Try a simple matrix

--- src/hello-world/hello.rs
+++ src/hello-world/hello.rs
@@ -1,4 +1,45 @@
+#[allow(dead_code)] // disable `dead_code` which warn against unused module
+struct Structure(i32);
+
+impl std::fmt::Display for Structure {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(f, "Structure({})", self.0)
+    }
+}
+
+#[derive(Debug)]
+#[allow(dead_code)]
+struct Point {
+    x: f64,
+    y: f64,
+}
+
 fn main() {
     println!("Hello, world!");
     println!("I am a Rustacean!");
+
+    println!("This struct `{}` will print now!", Structure(3));
+
+    // As can named arguments.
+    println!("{subject} {verb} {object}",
+             object="the lazy dog",
+             subject="the quick brown fox",
+             verb="jumps over");
+
+    // Different formatting can be invoked by specifying the format character
+    // after a `:`.
+    println!("Base 10:               {}",   69420); // 69420
+    println!("Base 2 (binary):       {:b}", 69420); // 10000111100101100
+    println!("Base 8 (octal):        {:o}", 69420); // 207454
+    println!("Base 16 (hexadecimal): {:x}", 69420); // 10f2c
+
+    println!("Hello {0} is {1:.5}", "x", 0.01);
+    format!("{:#?}", (100, 200));
+
+    let pi = 3.141592;
+    println!("Pi is roughly {pi:.3}");
+
+    let point = Point { x: 3.0, y: 4.0 };
+    println!("{:?}", point); // Debug formatting
+    println!("{:#?}", point); // Pretty Debug formatting
 }
---
+++ src/types/matrix.rs
@@ -0,0 +1,54 @@
+use std::fmt::*;
+
+// Tuples can be used as function arguments and as return values.
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+    // `let` can be used to bind the members of a tuple to variables.
+    let (int_param, bool_param) = pair;
+
+    (bool_param, int_param)
+}
+
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    imag: f64,
+}
+
+impl Display for Complex {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "{} + {}i", self.real, self.imag)
+    }
+}
+
+
+// The following struct is for the activity.
+#[derive(Debug)]
+#[allow(dead_code)]
+struct Matrix(f32, f32, f32, f32);
+
+impl Display for Matrix {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        write!(f, "({0:^6.2} {1:^6.2})\n({2:^6.2} {3:^6.2})", self.0, self.1, self.2, self.3)
+    }
+}
+
+fn main() {
+    let pair = (1, true);
+    println!("Pair is {:?}", pair);
+
+    println!("The reversed pair is {:?}", reverse(pair));
+
+    // Tuples can be destructured to create bindings.
+    let tuple = (1, "hello", 4.5, true);
+
+    let (a, b, c, d) = tuple;
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{:?}", matrix);
+    println!("{}", matrix);
+
+    let complex = Complex { real: 3.0, imag: 4.0 };
+    println!("{:?}", complex);
+    println!("{}", complex);
+}
